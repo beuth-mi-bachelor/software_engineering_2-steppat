37abb075edebc63d20ade4dbc08e8a403c510099
Duve, Michael <michael.duve@bild.de>
2014-11-20 18:06:18 +0100

    little styling for better view

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,28 +1,32 @@
-<header class="head top">
-    <a href="index.php"><img class="left logo" src="http://placehold.it/100x100" alt="Logo links"/></a>
+<header class="head top clearfix">
+    <a class="logo" href="index.php" target="_self"><img class="left logo" src="http://placehold.it/100x100" alt="Logo links"/></a>
     <nav>
         <h3 class="no-content">Menü</h3>
         <ul class="nav-list">
             <li class="nav-item">
-                <a href="index.php?action=contest-overview" target="_self">Übersicht Wettbewerbe</a>
+                Menu
+                <ul>
+                    <li class="nav-item">
+                        <a href="index.php?action=contest-overview" target="_self">Wettbewerbe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.php?action=contest-new" target="_self">Neuer Wettbewerb</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.php?action=idea-new" target="_self">Neue Idee</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.php?action=logout" target="_self">Logout</a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item">
-                <a href="index.php?action=contest-new" target="_self">Neuer Wettbewerb</a>
-            </li>
-            <li class="nav-item">
-                <a href="index.php?action=idea-new" target="_self">Idee einreichen</a>
-            </li>
-            <li class="nav-item">
-                <a href="index.php?action=logout" target="_self">Logout</a>
-            </li>
+
         </ul>
     </nav>
     <h1 class="lead-headline">Ideenportal für Reha-Geräte</h1>
 
-    <form class="search-wrapper" action="index.php/?action=search" method="post">
-        <fieldset class="fieldset-wrapper">
-            <input class="search-input" type="text" name="search" placeholder="Suchbegriff">
-            <input class="search-send" type="submit" name="submit" value="suchen">
-        </fieldset>
+    <form class="search-wrapper clearfix" action="index.php/?action=search" method="post">
+        <input class="search-input" type="text" name="search" placeholder="Suchbegriff">
+        <input class="search-send" type="submit" name="submit" value="suchen">
     </form>
 </header>
